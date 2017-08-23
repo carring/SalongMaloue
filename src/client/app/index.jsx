c1ce2efafe7beddbb5024c7a917b897d4c1c6269
Carolina Ring <carolina.ring@dynabyte.se>
2017-08-23 14:12:22 +0200

    Removed old imports

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import {render} from 'react-dom';
-import AwesomeComponent from './AwesomeComponent.jsx';
-import OtherComponent from './OtherComponent.jsx';
-import Button from './counter-button/counter-button.jsx';
 import ContainerComponent from './container/container.jsx';
 
 class App extends React.Component {
