a5982094bc7efd14c5a1d3638bcc21766cb899f3
Carolina Ring <carolina.ring@dynabyte.se>
2017-08-23 17:16:06 +0200

    modified dev config

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,18 +1,19 @@
 const path = require('path')
 const webpack = require('webpack')
+var BUILD_DIR = path.resolve(__dirname, 'public');
+var APP_DIR = path.resolve(__dirname, 'src/client/app');
 
 module.exports = {
   devtool: 'eval',
 
   entry: [
-    'webpack-hot-middleware/client',
-    './src/client/app/index.jsx'
+    APP_DIR + 'webpack-hot-middleware/client',
+    APP_DIR + '/index.jsx'
   ],
 
   output: {
-    path: path.join(__dirname, 'public'),
-    filename: 'bundle.js',
-    publicPath: '/public/'
+    path: BUILD_DIR,
+    filename: 'bundle.js'
   },
 
   plugins: [
